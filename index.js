b27daf372e7457a2d7744d14a10f591e82fa95d2
kvkens <kvkens@qq.com>
2018-01-03 13:37:06 +0800

    add:config

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 /**
  * proxy server for uba-server
- * Date : 2018-01-03 10:18:32
+ * Date : 2018-01-03 13:27:59
  */
 
 const Proxy = require("http-proxy-middleware");
 const c2k = require("koa2-connect");
 const Router = require("koa-router");
-// const bodyParser = require("koa-bodyparser");
+const chalk = require("chalk");
+const utils = require("./utils");
 
 
 module.exports = (app, opt) => {
   let router = new Router();
-  router.all("*", c2k(Proxy({
-    target: "https://cnodejs.org",
-    changeOrigin: true,
-    onProxyRes: (proxyRes) => {
-      proxyRes.headers["Uba-Server-Proxy"] = require("./package.json").version;
-    },
-    onProxyReq: (proxyReq, req, res) => {
-      console.log(`${proxyReq.method} - ${proxyReq.path}`);
-    }
-  })));
+  router.all("*", c2k(Proxy(
+    Object.assign({
+      target: "https://api.github.com/",
+      changeOrigin: true,
+      onProxyRes: (proxyRes) => {
+        proxyRes.headers["Uba-Server-Proxy"] = require("./package.json").version;
+      },
+      onProxyReq: (proxyReq, req, res) => {
+        console.log(chalk.green(`[${utils.getTime()}] [ProxyServer] : Method : ${proxyReq.method} -> Proxy : ${proxyReq.path}`));
+      }
+    }, opt)
+  )));
   app.use(router.routes());
-  // app.use(bodyParser());
 }
---
+++ utils.js
@@ -0,0 +1,7 @@
+
+exports.getTime = () => {
+    let time = new Date();
+    return `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
+  }
+  
+  
