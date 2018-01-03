1d93b96fc159ec391f66ce1b71cd70c2f588d7de
kvkens <kvkens@qq.com>
2018-01-03 10:20:04 +0800

    init

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 /**
  * proxy server for uba-server
- * Date : 2018-01-02 16:10:46
+ * Date : 2018-01-03 10:18:32
  */
 
 const Proxy = require("http-proxy-middleware");
 const c2k = require("koa2-connect");
 const Router = require("koa-router");
-const bodyParser = require("koa-bodyparser");
+// const bodyParser = require("koa-bodyparser");
 
 
-console.log(bodyParser)
 module.exports = (app, opt) => {
   let router = new Router();
   router.all("*", c2k(Proxy({
@@ -19,10 +18,9 @@ module.exports = (app, opt) => {
       proxyRes.headers["Uba-Server-Proxy"] = require("./package.json").version;
     },
     onProxyReq: (proxyReq, req, res) => {
-      console.log(proxyReq.method);
-      console.log(proxyReq.path);
+      console.log(`${proxyReq.method} - ${proxyReq.path}`);
     }
   })));
   app.use(router.routes());
-  app.use(bodyParser());
+  // app.use(bodyParser());
 }
